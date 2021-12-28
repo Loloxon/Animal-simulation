310a620d26c5e1b82ab54cad277613ba6ba13c02
loloxon <nikodemko@interia.pl>
2021-12-28 11:00:00 +0100

    gradle command workin

--- src/main/java/simulation/World.java
+++ src/main/java/simulation/World.java
@@ -2,7 +2,6 @@ package simulation;
 
 import javafx.application.Application;
 import simulation.gui.App;
-
 public class World {
     public static void main(String[] args){
         Application.launch(App.class);
